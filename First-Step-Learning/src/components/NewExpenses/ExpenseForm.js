587a5acda63d3ff9291a22d793753c8fa8cf7968
ShweSinHtay <shwesin@isgm2.com>
2022-01-31 14:22:59 +0630

    second learing prj added

--- First-Step-Learning/src/components/NewExpenses/ExpenseForm.js
+++ First-Step-Learning/src/components/NewExpenses/ExpenseForm.js
@@ -45,9 +45,9 @@ const ExpenseForm = (props) => {
     const submitHandler = (event) => {
         event.preventDefault();
         const expenseData = {
-            title: enteredTitle,
-            amount: enteredAmount,
-            date: new Date(enteredDate)
+          title: enteredTitle,
+          amount: +enteredAmount,
+          date: new Date(enteredDate),
         };
         props.onSaveExpenseData(expenseData);
         console.log(expenseData);
