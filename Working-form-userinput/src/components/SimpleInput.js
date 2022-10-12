10041117caf77af8979cdd84e7c1c9a50038d388
ShweSinHtay <shwesin@isgm2.com>
2022-10-13 00:31:32 +0900

    input form

---
+++ Working-form-userinput/src/components/SimpleInput.js
@@ -0,0 +1,32 @@
+import { useRef, useState } from "react";
+const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  };
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+    if(enteredName.trim() === ''){
+      return;
+    }
+    console.log("Enter name =>", enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log("Enter value :", enteredValue);
+    // nameInputRef.current.value = '';
+    setEnteredName(" ");
+  };
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className='form-control'>
+        <label htmlFor='name'>Your Name</label>
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName}/>
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
